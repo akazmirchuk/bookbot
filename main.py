f03f402cf3158866cb00855ae2108b8b6cb79082
akazmirchuk <andrew.kazmirchuk@gmail.com>
2024-12-31 09:45:52 -0500

    count chars mod

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ def main():
 
     print(file_contents)
     print(f"Word Count: {count_words(file_contents)}")
+    print(count_uchars(file_contents))
 
 def count_words(text):
     words = len(text.split())
     
     return words
+
+def count_uchars(text):
+    words = (text.lower()).split()
+    #words = words.split()
+    
+    print(words)
+    uncounted_chars = words.split()
+
+
+    chars = {}
+
+
+
 
 main()
 
