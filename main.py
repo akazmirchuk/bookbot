2cc8adf897aa94f82ec7567cd62cdcba7242768c
akazmirchuk <andrew.kazmirchuk@gmail.com>
2024-12-31 09:34:58 -0500

    count words mod

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def main():
         file_contents = f.read()
 
     print(file_contents)
+    print(f"Word Count: {count_words(file_contents)}")
+
+def count_words(text):
+    words = text.split()
+    return words
 
 main()
 
