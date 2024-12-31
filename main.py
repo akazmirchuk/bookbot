b2c9f83c5da5880b5918d86a45fd501a89275647
akazmirchuk <andrew.kazmirchuk@gmail.com>
2024-12-31 10:46:55 -0500

    report mod

--- main.py
+++ main.py
@@ -3,23 +3,17 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    file_text = file_contents
     word_count = count_words(file_contents)
     char_count = count_uchars(file_contents)
     report(word_count,char_count)
-    #print(f"Word Count: {count_words(file_contents)}")
-    #print(count_uchars(file_contents))
 
 def count_words(text):
-    words = len(text.split())
-    
+    words = len(text.split())  
     return words
 
 def count_uchars(text):
     
-    formatted_text = text.lower()
-    #chars = list(text)
-        
+    formatted_text = text.lower()  
     dict_chars = {}
 
     for c in formatted_text:
@@ -28,9 +22,16 @@ def count_uchars(text):
         else:
             dict_chars[c] += 1
     
+
+    def sort_on(dict):
+        return dict["num"]
+    
+    dict_chars.sort(reverse=True, key=sort_on)
+
     return dict_chars
 
 def report(word, char):
+
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{word} words found in the document\n\n")
     
